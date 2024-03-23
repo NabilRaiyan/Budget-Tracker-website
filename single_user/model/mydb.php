88e2226102d6e18010efdd838f89c7c6f9a1ce4c
NabilRaiyan <rraiyan77@gmail.com>
2024-03-23 15:28:04 +0600

    Adding update expense controller

--- single_user/model/mydb.php
+++ single_user/model/mydb.php
@@ -13,7 +13,6 @@
             return $result;
         }
 
-
         function Login($conn, $table, $email, $password){
             $sql = "SELECT email, password FROM $table WHERE email='$email' AND password = '$password'";
             $result = $conn->query($sql);
