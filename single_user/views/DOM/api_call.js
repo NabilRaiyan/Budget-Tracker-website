987d0fd8969954c74cd432694d5ab4c9faa1fb2b
NabilRaiyan <rraiyan77@gmail.com>
2024-04-04 22:04:34 +0600

    adding delete

--- single_user/views/DOM/api_call.js
+++ single_user/views/DOM/api_call.js
@@ -53,7 +53,6 @@ async function getJuz(){
 }
 
 
-
 // get all the surah button
 document.getElementById('get_ayah').addEventListener('click', getJuz);
 document.addEventListener('DOMContentLoaded', getSurahName);
