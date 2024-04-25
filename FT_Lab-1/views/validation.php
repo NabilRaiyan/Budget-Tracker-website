63fe9b908d8073278f796a71bc828af44caf99e5
NabilRaiyan <rraiyan77@gmail.com>
2024-04-25 23:34:25 +0600

    adding js validation

--- FT_Lab-1/views/validation.php
+++ FT_Lab-1/views/validation.php
@@ -10,31 +10,31 @@
     <div class="container">
         <h1>FinTech</h1>
         <form method="post" id="myForm" onsubmit="return validateForm()">
-            <div>
+            <div class="form-group">
                 <label for="username">Username:</label>
                 <input type="text" id="username" name="username">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="confirm_password">Confirm Password:</label>
                 <input type="password" id="confirm_password" name="confirm_password">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="email">Email:</label>
                 <input type="email" id="email" name="email">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="address">Address:</label>
                 <input type="text" id="address" name="address">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="phone">Phone Number:</label>
                 <input type="text" id="phone" name="phone">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="position">Position:</label>
                 <select id="position" name="position">
                     <option value="">Select Position</option>
@@ -43,15 +43,15 @@
                     <option value="manager">Manager</option>
                 </select>
             </div>
-            <div>
+            <div class="form-group">
                 <label for="annual_income">Annual Income:</label>
                 <input type="text" id="annual_income" name="annual_income">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="years_experience">Years of Experience:</label>
                 <input type="text" id="years_experience" name="years_experience">
             </div>
-            <div>
+            <div class="form-group">
                 <label for="industry">Industry:</label>
                 <select id="industry" name="industry">
                     <option value="">Select Industry</option>
@@ -61,8 +61,8 @@
                     <option value="Education">Education</option>
                 </select>
             </div>
-            <div>
-                <button type="submit">Submit</button>
+            <div class="form-group">
+                <button class="form-submit" type="submit">Submit</button>
             </div>
         </form>
     </div>
