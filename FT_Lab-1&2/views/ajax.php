d100b49e760a8d614ae900c668d531c163d11814
NabilRaiyan <rraiyan77@gmail.com>
2024-04-29 21:12:26 +0600

    adding styles to the form

--- FT_Lab-1&2/views/ajax.php
+++ FT_Lab-1&2/views/ajax.php
@@ -3,6 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- css file link -->
 <link rel="stylesheet" href="styles.css">
 <title>User Lookup</title>
 </head>
